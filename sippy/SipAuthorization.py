968a114fd951bab234dc56099d8d88b1b2a11245
Peter Lemenkov <lemenkov@gmail.com>
2010-08-27 15:30:19 +0400

    Use DigestCalcHA1 and DigestCalcResponse from Twisted
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- sippy/SipAuthorization.py
+++ sippy/SipAuthorization.py
@@ -23,8 +23,10 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
 from SipGenericHF import SipGenericHF
-from hashlib import md5
 from time import time
+
+from twisted.protocols.sip import DigestCalcHA1
+from twisted.protocols.sip import DigestCalcResponse
 
 class SipAuthorization(SipGenericHF):
     hf_names = ('authorization',)
@@ -131,45 +133,3 @@ class SipAuthorization(SipGenericHF):
             pass
         return False
 
-def DigestCalcHA1(pszAlg, pszUserName, pszRealm, pszPassword, pszNonce, pszCNonce):
-    m = md5()
-    m.update(pszUserName)
-    m.update(":")
-    m.update(pszRealm)
-    m.update(":")
-    m.update(pszPassword)
-    HA1 = m.digest()
-    if pszAlg == "md5-sess":
-        m = md5()
-        m.update(HA1)
-        m.update(":")
-        m.update(pszNonce)
-        m.update(":")
-        m.update(pszCNonce)
-        HA1 = m.digest()
-    return HA1.encode('hex')
-
-def DigestCalcResponse(HA1, pszNonce, pszNonceCount, pszCNonce, pszQop, pszMethod, pszDigestUri, pszHEntity):
-    m = md5()
-    m.update(pszMethod)
-    m.update(":")
-    m.update(pszDigestUri)
-    if pszQop == "auth-int":
-        m.update(":")
-        m.update(pszHEntity)
-    HA2 = m.digest().encode('hex')
-    m = md5()
-    m.update(HA1)
-    m.update(":")
-    m.update(pszNonce)
-    m.update(":")
-    if pszNonceCount and pszCNonce: # pszQop:
-        m.update(pszNonceCount)
-        m.update(":")
-        m.update(pszCNonce)
-        m.update(":")
-        m.update(pszQop)
-        m.update(":")
-    m.update(HA2)
-    response = m.digest().encode('hex')
-    return response
